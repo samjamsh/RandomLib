08f9dfd351373a4e8a30ead1a7183ed399f7d73a
Al Yasir <75454542+samjamsh@users.noreply.github.com>
2025-11-02 16:09:20 +0200

    Create example_code.py

---
+++ example_code.py
@@ -0,0 +1,10 @@
+from ramlib import genrandom
+
+print("Get & Print a random value")
+
+start = 1
+end = 10
+
+random_value = genrandom(start, end).getnew()
+print("\nRandom Value:", random_value)
+print()
